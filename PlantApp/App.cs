742e26d1d935a0358026b1d594fd2aef0e1d4de1
saraglavin <hej@saraglavin.se>
2019-01-09 15:24:30 +0100

    Skapat huvudmenyn och lagt till .domain.

--- PlantApp/App.cs
+++ PlantApp/App.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PlantApp.Domain;
 
 namespace PlantApp
 {
@@ -20,9 +21,29 @@ namespace PlantApp
 
         private void MainMenu()
         {
-            ShowPlantsMenu();
-            SeeUserPlantsMenu();
-            SearchTipsMenu();
+            Header("HuvudMeny");
+            WriteLine("a) Visa plantorna i databasen\n" +
+                "b) Visa användarplantor\n" +
+                "c) Sök bland tips\n");
+           
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.A)
+                ShowPlantsMenu();
+
+            if (key == ConsoleKey.B)
+                SeeUserPlantsMenu();
+
+            if (key == ConsoleKey.C)
+                SearchTipsMenu();
+
+            else
+            {
+                WriteLine("Nu blev det fel!");
+                Console.ReadKey();
+                MainMenu();
+            }
         }
 
         private void SearchTipsMenu()
--- PlantApp/DataAccess.cs
+++ PlantApp/DataAccess.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
+using PlantApp.Domain;
 
 namespace PlantApp
 {
